618b58cef990a43d743684118f4638114717711c
Divpreet Dhingra <divdhingra@Divpreets-Laptop.local>
2024-12-02 22:53:38 -0800

    Update GET request to unpack pet-attributes from sql query

--- server/server.py
+++ server/server.py
@@ -4,7 +4,6 @@
 # intercepted/'direct' API-requests)
 import re
 import json # Module to read JSON objects, and convert them to usable JS-objects
-import requests # To match fetch-requests to some API with Python :)
 
 import random # Module to generate random values
 
@@ -29,26 +28,27 @@ from collections import OrderedDict
 app = Flask(__name__)
 CORS(app)
 
-connection_string = f"""gssencmode=disable user={os.getenv("SUPABASE_USER")} password={os.getenv("SUPABASE_PASSWORD")} 
-                        host={os.getenv("SUPABASE_HOST")} port={os.getenv("SUPABASE_PORT")} 
-                        dbname={os.getenv("SUPABASE_DB_NAME")}"""
+connection_string = f""" gssencmode=disable 
+    user={os.getenv("SUPABASE_USER")} password={os.getenv("SUPABASE_PASSWORD")} host={os.getenv("SUPABASE_HOST")} 
+    port={os.getenv("SUPABASE_PORT") } dbname={os.getenv("SUPABASE_DB_NAME")} 
+"""
 
 conn = psycopg2.connect(connection_string)
 cursor = conn.cursor()
 
 # Users (non-serial attributes): UNIQUE username | is_activated_account | books_checked_out | books_overdue |
 # password_hash BYTEA NOT NULL, -- password for the user (hashed for security) [bytea-format]
-# Dummy to single user for now (just update / read from this user's specific endpoint [check discord for api-endpoitns])
+# # Dummy to single user for now (just update / read from this user's specific endpoint [check discord for api-endpoitns])
 CREATE_USERS_TABLE = (
     """
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY, -- Unique user identifier for the table (hence SERIAL | unrelated to input)
             user_name TEXT NOT NULL UNIQUE, -- username
             pet_health INT NOT NULL DEFAULT 100, -- pet's current health level | 0 - 100
-            pet_hunger INT NOT NULL DEFAULT 0, -- pet's current hunger level | 0 - 10
-            pet_mood INT NOT NULL DEFAULT 10 -- pet's current mood level | 0 - 10
-            creation_time DATETIME DEFAULT NOW() -- time account was created
-        );                        
+            pet_hunger INT NOT NULL DEFAULT 0, -- pet's current hunger level | 0 - 100
+            pet_mood INT NOT NULL DEFAULT 100, -- pet's current mood level | 0 - 100
+            creation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- time account was created [NOW() is only for SQL-queries of the table, not creating its initial instance]
+        );                    
     """
 )
 
@@ -64,7 +64,7 @@ def setInitialUsersTable():
         conn.commit() # Commit this change to GIT in supabase
         print("Initial Users Table Create") 
  
-setInitialUsersTable()
+setInitialUsersTable() 
 
 # Get details of this user's pet (health, hunger, mood)
 # NOTE: Get-requests don't allow a request-body (RESTful principles): Use url-query-params 
@@ -75,17 +75,11 @@ def getPetDetails():
         user_name = request.args.get("user_name") # from url-path query params
         cursor.execute("SELECT pet_health, pet_hunger, pet_mood from users WHERE user_name=%s", (user_name,)) 
 
-        # returns tuple of values
-        pet_health = cursor.fetchone()[0] 
-        pet_hunger = cursor.fetchone()[1]
-        pet_mood = cursor.fetchone()[2]
+        # Unpack tuple of values
+        pet_health, pet_hunger, pet_mood = cursor.fetchone()
 
         # '%s' parameterized query (psycopg2) to prevent SQL-string-injections attacks (i.e. sanitized via ` '/ ` escape before execution)
-        return jsonify({"petDetails": {"Health": pet_health, "Hunger": pet_hunger, "Mood": pet_mood}}), 200
+        return jsonify({"petDetails": {"health": pet_health, "hunger": pet_hunger, "mood": pet_mood}, "message": "Fetched pet details!"}), 200
     
     except Exception as e: # Handle database errors
         return jsonify({"error": str(e)}), 500       
-
-
-
-
