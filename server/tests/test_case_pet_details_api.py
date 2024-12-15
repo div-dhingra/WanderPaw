97699f88f0b336b2ede7c5b4ef7a5b82392e0ccc
Divpreet Dhingra <divdhingra@Divpreets-Laptop.local>
2024-12-14 18:41:23 -0800

    Initialize tests folder + Create API unit test for pet-details endpoint GET request

---
+++ server/tests/test_case_pet_details_api.py
@@ -0,0 +1,51 @@
+# API-Endpoint Test #1: 
+
+# Import flask app that all apis are bundled into for test
+from server import app
+from datetime import datetime
+
+# Command to run tests (return True/False in console):
+# pytest tests/test_api.py
+
+# 2 tests per api (good + faulty [expect faulty error response, ex. args not provided, etc.])
+
+def test_case_1():
+    client = app.test_client()
+    response = client.get("/api/users/pet-details?user_name=andrews_covalent_bond")
+
+    data = response.json
+
+    # Verify HTTP-ok status
+    assert response.status_code == 200
+
+    # Verify the top-level keys
+    assert "petDetails" in data
+    assert "message" in data
+
+    # Verify pet_details structure (i.e. all keys exists in response)
+    pet_details = data["petDetails"]
+    assert "health" in pet_details
+    assert "hunger" in pet_details
+    assert "mood" in pet_details
+
+    # Verify expected, specific values
+    assert data["message"] == "Fetched pet details!"
+
+    pet_health = pet_details.get("health")
+    pet_hunger, hunger_last_updated = pet_details.get("hunger")
+    pet_mood, mood_last_updated = pet_details.get("mood")
+
+    # Verify data-type of each pet detail 
+    # (values are dynamic, but data-types are static so we can uniformly test them)
+    assert isinstance(pet_health, int)  
+    assert isinstance(pet_hunger, int)  
+    assert isinstance(pet_mood, int)  
+    assert datetime.strptime(hunger_last_updated, '%a, %d %b %Y %H:%M:%S GMT') # If conversion to datetime object is valid,
+    assert datetime.strptime(mood_last_updated, '%a, %d %b %Y %H:%M:%S GMT')   # FOR THIS PROVIDED FORMAT, then the string is 
+                                                                               # a valid 'TIMESTAMP'-PSQL object.
+                                                                               # Else, invalid string.
+
+    # Verify that health, hunger, mood integer-attributes are within range (0 - 100)
+    assert 0 <= pet_health <= 100
+    assert 0 <= pet_hunger <= 100
+    assert 0 <= pet_mood <= 100
